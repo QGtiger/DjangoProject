bb882eecb720dc9244bd08694ff250c06976633e
Qgtiger <142686337@qq.com>
2019-03-23 23:42:10 +0800

    modified

---
+++ LightBlog/article/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,re_path
+from . import views
+
+app_name = 'article'
+
+urlpatterns = [
+    path(r'article_column/', views.article_column, name='article_column'),
+    path('rename_article_column/', views.rename_article_column, name="rename_article_column"),
+    path('del_article_column/', views.del_article_column, name="del_article_column"),
+    path('article_post/',views.article_post,name="article_post"),
+    path('article_list',views.article_list,name="article_list"),
+    re_path('article_detail/(?P<id>\d+)/', views.article_detail, name="article_detail"),
+    path('del-article/', views.del_article, name="del_article"),
+    path('redit-article/<int:article_id>/', views.redit_article, name="redit_article"),
+]
