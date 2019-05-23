94aa8fda89e5b96e8ffcf0e6571f0a21340e811c
QGtiger <1426286337@qq.com>
2019-05-23 19:59:38 +0800

    前端接收sessionid并存放在cookie

--- djangoVue/backend/views.py
+++ djangoVue/backend/views.py
@@ -11,20 +11,26 @@ from django.contrib.auth.decorators import login_required
 @csrf_exempt
 def loginView(request):
     if request.method == 'POST':
+        print(request.POST)
         username = request.POST.get('username','error')
         password = request.POST.get('password','error')
+        print(username,password)
         tips = ''
         if User.objects.filter(username=username):
             # authenticate 判断密码是否正确
             user = authenticate(username=username, password=password)
             if user:
                 if user.is_active:
-                    tips = '登陆成功'
+                    login(request, user)
+                    tips = '登陆成功,sessionId 为'+str(request.session.session_key)
+                    res = HttpResponse(json.dumps({'tips':tips, 'sesssionid': request.session.session_key}))
+                    return res
             else:
                 tips = ' 密码错误，请重新输入 '
         else:
             tips = ' 用户不存在，请注册 '
-        return HttpResponse(json.dumps({'tips': tips}))
+        res = HttpResponse(json.dumps({'tips': tips}))
+        return res
     else:
         return HttpResponse('asd')
 
@@ -41,6 +47,6 @@ def register(request):
             user.save()
             tips = ' 注册成功 '
         tip2 = '无人登录'
-        if request.user.username:
-            tip2 = request.user.username + '在登录'
+        if request.session.get('username',''):
+            tip2 = request.session.get('username','') + '在登录'
         return HttpResponse(json.dumps({'tips':tips, 'User':tip2}))
--- djangoVue/frontend/src/components/account/Login.vue
+++ djangoVue/frontend/src/components/account/Login.vue
@@ -2,39 +2,41 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div>
-        <label for="username">Username</label>
-        <Input v-model="username" placeholder="Enter name" id="username" style="width: auto" />
+      <label for="username">Username</label>
+      <Input v-model="username" placeholder="Enter name" id="username" style="width: auto"/>
     </div>
     <div>
-        <label for="password">Password</label>
-        <Input v-model="password" placeholder="Enter password" id="password" style="width: auto" />
+      <label for="password">Password</label>
+      <Input v-model="password" placeholder="Enter password" id="password" style="width: auto"/>
     </div>
-    <br><br>
+    <br>
+    <br>
     <Button type="primary" @click="handleLogin" key="login">登录</Button>
   </div>
 </template>
 
 <script>
-import $ from '@/jquery';
+import $ from "@/jquery";
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      username: '',
-      password: '',
-    }
+      msg: "Welcome to Your Vue.js App",
+      username: "",
+      password: ""
+    };
   },
   methods: {
-    handleLogin () {
+    handleLogin() {
       var _this = this;
       var username = _this.username;
       var password = _this.password;
-      if(!(username && password)){
+      if (!(username && password)) {
         this.$Modal.error({
           title: "错误",
           content: "用户名和密码不能为空"
-        })
+        });
         return;
       }
       var data = {
@@ -42,18 +44,33 @@ export default {
         password: _this.password
       };
       console.log(data);
-      jQuery.ajax({
+      // jQuery.ajax({
+      //   url: "http://127.0.0.1:8000/backend/login",
+      //   type: "POST",
+      //   data: data,
+      //   dataType: 'json',
+      //   success: function(e){
+      //     _this.msg = e;
+      //   }
+      // })ma
+      const _data = new FormData();
+      _data.append("username", _this.username);
+      _data.append("password", _this.password);
+      axios({
+        method: "post",
         url: "http://127.0.0.1:8000/backend/login",
-        type: "POST",
-        data: data,
-        dataType: 'json',
-        success: function(e){
-          _this.msg = e;
-        }
-      })
+        data: _data,
+        // withCredentials: true
+      }).then(res => {
+        console.log(res.data);
+        _this.msg = res.data;
+        const { sesssionid } = res.data;
+        localStorage.setItem("sesssionid", sesssionid);
+        document.cookie = `sesssionid=${sesssionid};`
+      });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
